7d51bb04ab48b48e11aab3968b63664f2de872cf
kamalesh <kamalesh@skyraan.com>
2026-05-02 14:54:36 +0530

    revise in mysql2 to server

--- configs/db.js
+++ configs/db.js
@@ -1,17 +1,17 @@
 import mysql from "mysql2";
 
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "rightshadow_user",
-//   password: "Nm^VOyCZ!@e9S8Yq",
-//   database: "rightshadow_db",
-// });
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "",
-  database: "right_shadow",
+  user: "rightshadow_user",
+  password: "Nm^VOyCZ!@e9S8Yq",
+  database: "rightshadow_db",
 });
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "right_shadow",
+// });
  
 db.query("SET time_zone = '+05:30'");
 
