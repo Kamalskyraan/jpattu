dcabf8c10f93dccf5bbb7e385f23a5f0c3c106a2
kamalesh <kamalesh@skyraan.com>
2026-05-12 15:25:14 +0530

    changes in parent

--- controllers/tree.controller.js
+++ controllers/tree.controller.js
@@ -126,7 +126,7 @@ export const getTreeChartForTT = async (req, res) => {
         u.user_id,
         u.name,
         r.level,
-        r.ancestor_id
+        r.ancestor_id as parent
       FROM users u
       INNER JOIN tt_user_relations r
         ON u.user_id = r.descendant_id
@@ -170,7 +170,7 @@ export const getTreeForTT = async (req, res) => {
     } else {
       const tree = buildTree(data, id);
 
-      res.status(200).json({ data: tree , message : "abcd"});
+      res.status(200).json({ data: tree, message: "abcd" });
     }
   } catch (err) {
     console.log(err);
