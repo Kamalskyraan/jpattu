7ebdf6b761a91872626836b122e4527df5499fc8
kamalesh <kamalesh@skyraan.com>
2026-06-10 16:17:15 +0530

    changes in tree view

--- routers/TTusers.js
+++ routers/TTusers.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getTTUserName } from "../controllers/users.controller.js";
+import { getTTUser, getTTUserName } from "../controllers/users.controller.js";
+import { verifyUser } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.get("/", getTTUserName);
-
+router.get("/:user_id", verifyUser, getTTUser);
 export default router;
