5125717b3608fd7c14f0409b1ca7aaec44a0e226
kamalesh <kamalesh@skyraan.com>
2026-06-17 17:08:31 +0530

    changes in right shadow backend

--- routers/UserTTBalance.js
+++ routers/UserTTBalance.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   getTTBalanceLogs,
+  getTTLevelIncome,
   updateTTBalanceStatus,
 } from "../controllers/userbalance.controller.js";
 import { verifyAdmin, verifyUser } from "../middlewares/auth.js";
@@ -9,4 +10,5 @@ const router = express.Router();
 
 router.get("/", verifyUser, getTTBalanceLogs);
 router.put("/", verifyAdmin, updateTTBalanceStatus);
+router.get("/income/:user_id", verifyUser, getTTLevelIncome);
 export default router;
