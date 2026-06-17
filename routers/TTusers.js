ae4aa23cde21270af04adaa59f1112f58a6d25fb
kamalesh <kamalesh@skyraan.com>
2026-06-17 18:31:44 +0530

    changes in update

--- routers/TTusers.js
+++ routers/TTusers.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getTTUser, getTTUserName } from "../controllers/users.controller.js";
+import { getTTUser, getTTUserName, updateTTUser } from "../controllers/users.controller.js";
 import { verifyUser } from "../middlewares/auth.js";
+import { updateValidation } from "../validator/authValidator.js";
 
 const router = express.Router();
 
 router.get("/", getTTUserName);
 router.get("/:user_id", verifyUser, getTTUser);
+router.put("/", verifyUser, updateValidation, updateTTUser);
 export default router;
